ee16b644fe27610978ceaea0c69253b7979e84de
Cauan Oliveira <cauano112@gmail.com>
2022-06-13 11:47:53 -0300

    criacao da home

---
+++ Models/Funcionario.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APPRH.Models
+{
+    public class Funcionario
+    {
+        public int IdFuncionario { get; set; }
+
+        public String Nome { get; set; }
+    }
+}
